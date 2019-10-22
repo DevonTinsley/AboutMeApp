1984b6c8b4a84a9fb71ae67dcb497d5831116b14
Devon Tinsley <dwt2307@gmail.com>
2019-10-21 22:55:24 -0500

    Added ugly naviation, it looks like Sloth from goonies the thing, will prettify later

---
+++ AboutMe/AboutMe/BiographyPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AboutMe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BiographyPage : ContentPage
+    {
+        public BiographyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- AboutMe/AboutMe/MainPage.xaml.cs
+++ AboutMe/AboutMe/MainPage.xaml.cs
@@ -17,5 +17,20 @@ namespace AboutMe
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BiographyPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SkillsPage());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ContactPage());
+        }
     }
 }
